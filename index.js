6c13fab49d46cbbef3fe0a72cae9d823589abed1
Syd Bailey <sydabailey@gmail.com>
2019-07-19 00:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 // var katzDeliLine = []
-
-var some_obscure_line = ["a", "b"]
-var some_wierd_name = "c"
-
-function pushNameIntoLine(line, name) {
-    line.push(name)
-}
-
-pushNameIntoLine(some_obscure_line, some_wierd_name)
-
-var anotherLIne = []
-
-pushNameIntoLine(anotherLIne, some_wierd_name)
-
 // This function should take the line, and the name
 // of a person in line, and tell them what position
 // they are in line.
 // takes two parameters
 // - parameter line: an array of names, describing the current line
 // - parameter name: the person to join the line
+var katzDeliline =["Ada", "Grace"]
 function takeANumber(line, name) {
   // var katzDeliLine = ["Ada", "Grace"]
   // what's in the line?
@@ -30,5 +17,29 @@ function takeANumber(line, name) {
   var number = line.length
 
   return `Welcome, ${name}. You are number ${number} in line.`
-
 }
+
+// function nowServing (line){
+//   if (line.length === 0) {
+//     return "There is nobody waiting to be served"
+//   } else {
+//     return line.shift()
+//   }
+// }
+
+function currentLine (line) {
+  if (line.length === 0) {
+      return "The line is currently empty."  
+  } else {
+    // start your string
+
+    // build your string:
+    // for each thing in line , add a little bit to string
+
+    // return your string
+}
+
+// returns
+"The line is currently: [Object array]"
+"The line is currently: ["Ada", "Grace"]" 
+
