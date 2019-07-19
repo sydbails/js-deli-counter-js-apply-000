9cb9c8215893f88b8c7e3f3913b503c083e83632
Syd Bailey <sydabailey@gmail.com>
2019-07-19 00:51:42 +0000

    Automatically backed up by Learn

--- example.js
+++ example.js
@@ -1,14 +1,18 @@
 
 
 function doSomething(array) {
-  // return "something"
-
   var string = "hello"
   if (array.length === 0) {
     string = "I'm empty"
   }
 
+  for (var i = 0; i < array.length; i++) {
+    var item = array[i]
+  }
+
   return string
-
-
 }
+
+doSomething([]) // "I'M empty"
+
+doSomething([1,2,4]) // "1,2,4"
